0f237cc9d3759e83fe76d0b2a7f24313999c2d14
黄武韬 <346792184@qq.com>
2018-09-26 15:09:48 +0800

    修复windowsbug

--- config/importModules.js
+++ config/importModules.js
@@ -1,7 +1,8 @@
+var path = require('path')
+
 module.exports = function() {
-  var list = this.context.split('/')
-  const page = list.pop()
-  const dir = list.pop()
+  const page = path.basename(this.context)
+  const dir = path.basename(path.dirname(this.context))
   let source = `
   const page = require('./index.vue');
   window.__pages = {
